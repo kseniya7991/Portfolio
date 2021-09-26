2c53da1fd01841ef7c84e9f10b22a9a309f819ab
Kseniya Stoychikova <kseniya7991@gmail.com>
2021-09-26 17:43:32 +0300

    add project structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.scss';
+
+function Header() {
+  return (
+    <header className="Header">
+      <h1 className="Header__name">Portfolio</h1>
+    </header>
+  );
+}
+
+export default Header;
