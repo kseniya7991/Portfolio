7a041470512e331600bc002d521a215ac9782b68
Kseniya Stoychikova <kseniya7991@gmail.com>
2021-09-27 15:21:25 +0300

    add header with navigation block

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import './Header.scss';
+import Navigation from '../Navigation/Navigation';
 
 function Header() {
   return (
     <header className="Header">
       <h1 className="Header__name">Portfolio</h1>
+      <Navigation></Navigation>
     </header>
   );
 }
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import './Navigation.scss';
+
+function Navigation() {
+  return (
+    <ul className="Navigation">
+      <li className="Navigation__item">About</li>
+      <li className="Navigation__item">Projects</li>
+      <li className="Navigation__item">Stack</li>
+      <li className="Navigation__item">CV</li>
+      <li className="Navigation__item">Contact</li>
+    </ul>
+  );
+}
+
+export default Navigation;
