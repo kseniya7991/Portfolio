7b00798d6b7dd0d12cd41ecb6d93dd5ffb8f4bda
Kseniya Stoychikova <kseniya7991@gmail.com>
2021-09-27 18:21:17 +0300

    add burger menu block

---
+++ src/components/BurgerNav/BurgerNav.js
@@ -0,0 +1,13 @@
+import './BurgerNav.scss';
+
+function BurgerNav() {
+  return (
+    <ul className="BurgerNav">
+      <li className="BurgerNav__stick"></li>
+      <li className="BurgerNav__stick"></li>
+      <li className="BurgerNav__stick"></li>
+    </ul>
+  );
+}
+
+export default BurgerNav;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import './Header.scss';
-import Navigation from '../Navigation/Navigation';
+/* import Navigation from '../Navigation/Navigation'; */
+import BurgerNav from '../BurgerNav/BurgerNav';
 
 function Header() {
   return (
     <header className="Header">
       <h1 className="Header__name">Portfolio</h1>
-      <Navigation></Navigation>
+      {/* <Navigation></Navigation> */}
+      <BurgerNav></BurgerNav>
     </header>
   );
 }
