480a9cd9d8501cfcce489ab9b9e71eb1b9f1c311
Kseniya Stoychikova <kseniya7991@gmail.com>
2021-09-28 13:35:44 +0300

    add burger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import './Header.scss';
-/* import Navigation from '../Navigation/Navigation'; */
+import Navigation from '../Navigation/Navigation';
 import BurgerNav from '../BurgerNav/BurgerNav';
 
 function Header() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleWindowResize = () => {
+    if (window.innerWidth > 768) {
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+    }
+  };
+
+  useEffect(() => {
+    handleWindowResize();
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+
+
   return (
     <header className="Header">
       <h1 className="Header__name">Portfolio</h1>
-      {/* <Navigation></Navigation> */}
-      <BurgerNav></BurgerNav>
+      <Navigation isMobile={isMobile}></Navigation>
+      <BurgerNav isMobile={isMobile}></BurgerNav>
     </header>
   );
 }
