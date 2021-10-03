92642aa62dc995f88e61f900439612361b6e9dc0
Kseniya Stoychikova <kseniya7991@gmail.com>
2021-10-03 16:58:15 +0300

    edit  mouseMove function

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,50 +4,68 @@ import StyledLine from '../Permanent components/StyledLine/StyledLine';
 import './About.scss';
 
 function About() {
+  const circle = document.querySelector('.Photo__circle');
+  const circlePhoto = document.querySelector('.Photo__circle');
+  const photoProfile = document.querySelector('.About__photo');
+  /*   const photoProfileX = photoProfile.getBoundingClientRect().x;
+  const photoProfileY = photoProfile.getBoundingClientRect().y;
+  const photoWidth = 200; */
+  console.log(photoProfile, circle, circlePhoto);
+  /*
+  function getCoordinate(axis, indent, mouseCoordinate) {
+    const factor = (indent * 2) / photoWidth;
+    const difference = (mouseCoordinate - axis === 'X' ?
+    photoProfileX : photoProfileY) * (-1) * factor + indent;
+    let coordinate = 0;
+    console.log(axis === 'X' ? photoProfileX : photoProfileY);
+
+    if (difference > indent) {
+      coordinate = indent;
+    } else if (difference < -indent) {
+      coordinate = -indent;
+    } else {
+      coordinate = indent;
+    }
+    return coordinate;
+  }
+
+  function countTranslate(mouseX, mouseY) {
+    const coordinateX = getCoordinate('X', 8, mouseX);
+    const coordinateY = getCoordinate('Y', 10, mouseY);
+         const differenceX = (mouseX - photoProfileX) * (-1) * 0.08 + 8;
+    const differenceY = (mouseY - photoProfileY) * (-1) * 0.1 + 10;
+
+    let coordinateX = 0;
+    let coordinateY = 0;
+
+    if (differenceX > 8) {
+      coordinateX = 8;
+    } else if (differenceX < -8) {
+      coordinateX = -8;
+    } else {
+      coordinateX = differenceX;
+    }
+
+    if (differenceY > 10) {
+      coordinateY = 10;
+    } else if (differenceY < -10) {
+      coordinateY = -10;
+    } else {
+      coordinateY = differenceY;
+    }
+
+    return `translate(${coordinateX}px, ${coordinateY}px)`;
+  }
+
   function mouseMove(event) {
     const mouseX = event.pageX;
     const mouseY = event.pageY;
-    const profilePhoto = document.querySelector('.About__photo');
-    const profilePhotoX = profilePhoto.getBoundingClientRect().x;
-    const profilePhotoY = profilePhoto.getBoundingClientRect().y;
-    const photoCircle = document.querySelector('.Photo__circle');
+    console.log(countTranslate(mouseX, mouseY));
 
-    function countTranslate() {
-      const differenceX = (mouseX - profilePhotoX) * (-1) * 0.07 + 7;
-      let coordinateX = 0;
-      if (differenceX > 7) {
-        coordinateX = 7;
-      } else if (differenceX < -7) {
-        coordinateX = -7;
-      } else {
-        coordinateX = differenceX;
-      }
-
-      const differenceY = (mouseY - profilePhotoY) * (-1) * 0.1 + 10;
-      let coordinateY = 0;
-      if (differenceY > 10) {
-        coordinateY = 10;
-      } else if (differenceY < -10) {
-        coordinateY = -10;
-      } else {
-        coordinateY = differenceY;
-      }
-
-      if (differenceY > -6.5 && differenceY < 6.5) coordinateX = -9;
-
-      console.log(coordinateY, coordinateX);
-
-      return `translate(${coordinateX}px, ${coordinateY}px)`;
-    }
-
-    photoCircle.classList.remove('Photo__circle_hidden');
-    photoCircle.style.transform = countTranslate();
-
-    /*    console.log(profilePhotoY, mouseY); */
+    circle.style.transform = countTranslate(mouseX, mouseY);
   }
 
-  document.addEventListener('mousemove', mouseMove);
-
+  document.addEventListener('mousemove', mouseMove); */
   return (
     <>
       <StyledLine />
