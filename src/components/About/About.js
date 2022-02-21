1d634e7e120c3b40699340823b88b33a4394be9b
Kseniya St <kseniya7991@gmail.com>
2022-02-21 14:56:11 +0300

    fix about paragraph

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,10 @@
 import React, { useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedinIn, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
+import {
+  faLinkedinIn,
+  faInstagram,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons';
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import StyledLine from '../Permanent components/StyledLine/StyledLine';
 
@@ -32,7 +36,10 @@ function About() {
                   24
                 </li>
                 <li className="Details__item">
-                  <FontAwesomeIcon icon={faMapMarkerAlt} className="Details__icon" />
+                  <FontAwesomeIcon
+                    icon={faMapMarkerAlt}
+                    className="Details__icon"
+                  />
                   <span className="Details__name">Place:</span>
                   Minsk
                 </li>
@@ -42,63 +49,121 @@ function About() {
                 </li>
               </ul>
               <ul className="Details__social-networks">
-                <a href="https://www.instagram.com/ksunya_st_/" target="_blank" rel="noreferrer" label="inst: ksunya_st_" className="Social-item_link">
+                <a
+                  href="https://www.instagram.com/ksunya_st_/"
+                  target="_blank"
+                  rel="noreferrer"
+                  label="inst: ksunya_st_"
+                  className="Social-item_link"
+                >
                   <li className="Details__social-item">
-                    <FontAwesomeIcon icon={faInstagram} className="Social-item__icon" />
+                    <FontAwesomeIcon
+                      icon={faInstagram}
+                      className="Social-item__icon"
+                    />
                   </li>
-
                 </a>
-                <a href="https://www.linkedin.com/in/kseniya-stoychikova-907594201/" target="_blank" rel="noreferrer" label="linked in: Kseniya Stoichykova" className="Social-item_link">
+                <a
+                  href="https://www.linkedin.com/in/kseniya-stoychikova-907594201/"
+                  target="_blank"
+                  rel="noreferrer"
+                  label="linked in: Kseniya Stoichykova"
+                  className="Social-item_link"
+                >
                   <li className="Details__social-item">
-                    <FontAwesomeIcon icon={faLinkedinIn} className="Social-item__icon" />
+                    <FontAwesomeIcon
+                      icon={faLinkedinIn}
+                      className="Social-item__icon"
+                    />
                   </li>
-
                 </a>
-                <a href="https://github.com/kseniya7991" target="_blank" rel="noreferrer" label="github: kseniya7991" className="Social-item_link">
+                <a
+                  href="https://github.com/kseniya7991"
+                  target="_blank"
+                  rel="noreferrer"
+                  label="github: kseniya7991"
+                  className="Social-item_link"
+                >
                   <li className="Details__social-item">
-                    <FontAwesomeIcon icon={faGithub} className="Social-item__icon" />
+                    <FontAwesomeIcon
+                      icon={faGithub}
+                      className="Social-item__icon"
+                    />
                   </li>
-
                 </a>
-                <a href={CV} download target="_blank" rel="noreferrer" label="download the CV" className="Social-item_link">
+                <a
+                  href={CV}
+                  download
+                  target="_blank"
+                  rel="noreferrer"
+                  label="download the CV"
+                  className="Social-item_link"
+                >
                   <li className="Details__social-item Social-item_cv">
                     <span className="Social-item_cv_text">CV</span>
                   </li>
-
                 </a>
               </ul>
             </div>
           </div>
           <div className="About__block">
             <p className="About__paragraph">
-              I am a junior front-end developer with 1 year
-              training hands-on experience. I am a graduate of the Yandex.Praktikum
-              platform as a&nbsp;
+              I am a junior front-end developer with 1 year training hands-on
+              experience. I am a graduate of the Yandex.Praktikum platform as
+              a&nbsp;
               <span className="About__paragraph_high">
-                <a href="https://disk.yandex.by/d/69Q1IfByLv1DpQ" className="About__link" target="_blank" label="YP sertificate" rel="noreferrer">front-end developer</a>
+                <a
+                  href="https://disk.yandex.by/d/69Q1IfByLv1DpQ"
+                  className="About__link"
+                  target="_blank"
+                  label="YP sertificate"
+                  rel="noreferrer"
+                >
+                  front-end developer
+                </a>
               </span>
-              . Also I am a graduate of the Belarusian
-              State Academy of Communications as a marketer-economist.
+              . Also I am a graduate of the Belarusian State Academy of
+              Communications as a marketer-economist. My experience in this
+              field is 2.5 years. I worked as a Marketing Manager for 1.5 years
+              and as a Lead Digital Marketing Manager for 1 year.
             </p>
             <p className="About__paragraph">
               <em>But, I am keen on the code.</em>
             </p>
             <p className="About__paragraph">
-              I have hands-on experienced in back-end development. I prefer working in
-              a big or small team where everyone can learn and share experience.
-              I like to work on projects that have a specific goal and plan.
+              I have hands-on experienced in back-end development. I prefer
+              working in a big or small team where everyone can learn and share
+              experience. I like to work on projects that have a specific goal
+              and plan.
             </p>
             <p className="About__paragraph">
               <em>
-                What about my hobby&#63;
-                I&apos;m a&nbsp;
+                What about my hobby&#63; I&apos;m a&nbsp;
                 <span className="About__paragraph_high">
-                  <a href="https://probeg.org/user/4148/" className="About__link" target="_blank" label="PRObeg profile" rel="noreferrer">
+                  <a
+                    href="https://probeg.org/user/4148/"
+                    className="About__link"
+                    target="_blank"
+                    label="PRObeg profile"
+                    rel="noreferrer"
+                  >
                     runner
                   </a>
                 </span>
-                &nbsp;from
-                &lt;header&gt; to &lt;footer&gt;.
+                &nbsp;from &lt;header&gt; to &lt;footer&gt;. I have over then
+                16,000 km of running on my&nbsp;
+                <span className="About__paragraph_high">
+                  <a
+                    href="https://www.strava.com/athletes/11878652"
+                    className="About__link"
+                    target="_blank"
+                    label="Strava profile"
+                    rel="noreferrer"
+                  >
+                    strava profile
+                  </a>
+                </span>
+                .
               </em>
             </p>
           </div>
